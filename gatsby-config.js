d0d513a9cbc5c491874bd66f6bd6c6f30ebcf95d
Barrón <31053479+JhonatanBarron@users.noreply.github.com>
2020-12-25 12:58:51 -0600

    Retro to basic

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-plugins: [
-    {
-      resolve: `gatsby-plugin-google-adsense`,
-      options: {
-        publisherId: `ca-pub-8354409704567755`
-      },
-    },
-]
--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Cintillo from "../components/cintillo"
-
-
-export default function Home() {
-  return (
-		<Cintillo />
-  )
-}
